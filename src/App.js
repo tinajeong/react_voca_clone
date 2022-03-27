a7ac749dde85e8df78b99f94cc58ccf770a64af1
Tina <migujeong@gmail.com>
2022-03-28 06:05:14 +0900

    summary chap7 with useState

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import './App.css';
 import Hello from './component/Hello';
 
 function App() {
-  return <div className="App">
-    <Hello/>
-  </div>
+  return (
+    <div className="App">
+      <Hello />
+      <Hello />
+      <Hello />
+    </div>
+  );
 }
 
 export default App;
