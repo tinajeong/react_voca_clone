c9d540960c50e3d5e8918f643b1d5cd587b53d02
Tina <migujeong@gmail.com>
2022-03-28 08:22:58 +0900

    summary chap15 with applying creeate(POST)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React from "react";
 import Day from "./component/Day";
 import DayList from "./component/DayList";
 import Header from "./component/Header";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import EmptyPage from "./component/EmptyPage";
+import CreateWord from "./component/CreateWord";
+import CreateDay from "./component/CreateDay";
 
 function App() {
   return (
@@ -17,6 +19,12 @@ function App() {
             </Route>
             <Route path="/day/:day">
               <Day />
+            </Route>
+            <Route path="/create_word">
+              <CreateWord />
+            </Route>
+            <Route path="/create_day">
+              <CreateDay />
             </Route>
             <Route>
               <EmptyPage />
--- src/component/Header.js
+++ src/component/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
 
 export default function Header() {
@@ -8,12 +8,12 @@ export default function Header() {
         <Link to="/">토익 영단어(고급)</Link>
       </h1>
       <div className="menu">
-        <a href="#x" className="link">
+        <Link to="/create_word" className="link">
           단어 추가
-        </a>
-        <a href="#x" className="link">
+        </Link>
+        <Link to="/create_day" className="link">
           Day 추가
-        </a>
+        </Link>
       </div>
     </div>
   );
