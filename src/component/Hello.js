8c68d8e776b59ed02692e56a927c34b6afd0e1eb
Tina <migujeong@gmail.com>
2022-03-27 23:33:47 +0900

    apply css module in chap 5

--- src/component/Hello.js
+++ src/component/Hello.js
@@ -1,5 +1,5 @@
-import "./Hello.css";
+import styles from "./Hello.module.css";
 
 export default function Hello() {
-    return  <div className="box"><h2>Hello</h2></div>
+    return  <div className={styles.box}><h2>Hello</h2></div>
 }
